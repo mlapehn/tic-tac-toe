662676cd6bf7e19bdd10921ff8e4d035994a0143
mattchew <mlapehn@gmail.com>
2016-06-27 19:20:13 -0600

    Final game

--- js/js.js
+++ js/js.js
@@ -1,75 +1,75 @@
-// Function to start game // 
-function startGame(sg) {
-    document.turn = "X";
-    // Need if statement to clear board //
-    if (square.innerHTML === "X" && "O"); {
-
-    }
-    // if square is = to x and or o reset //
-    // document.winner = null; //
-}
-setMessage(document.turn + " get's to start."); // setMessage not working
-// Funciton to display message //
-// Needs work//
-function setMessage(msg) {
-       document.ElementsByClassName("messageBoard").innerHTML = msg;
-    }
-      //  Function to get next move //
-
-function nextMove(square) {
-        if (square.innerHTML === "") {
-            square.innerHTML = document.turn;
-            switchTurn();
-        } else {
-            alert("No cheating!!");
+// Function to start the game // 
+function startGame() {
+        
+            for (var i = 1; i <= 9; i = i + 1) {
+                clearBox(i);
+            }
+        
+            document.turn = "X";
+            if (Math.random() < 0.5) {
+                document.turn = "O";
+            }
+            document.winner = null;
+            setMessage(document.turn + " gets to start."); //message to alert player's whose turn it is // 
         }
-    }
-    // Function to switch turn //
-
-function switchTurn(swt) {
-    if (document.turn == "X") {
-        //alert("Player 1's turn!")
-        document.turn = "O";
-        //alert("Player 2's turn!.")
-    } else {
-        document.turn = "X";
-        //alert("Player 1's turn!")
-    }
-    // Set message goes here
-}
-
-// function checkRow(a, b, c, move) {
-//        var result = false;
-//        if (getbox(a) == move && getbox(b) == move && getbox(c) == move) {
-//            result = true;
-//        }
-//        return result;
-//    }
-    // restart stuff //
-    // button doesn't work //
-
-
-function restartGame(restartGame){
-
-}
-    // checks for winner // 
-    // Still not working //
-    //function checkForWinner(move) {
-    //  var result = false;
-    //  if (checkRow(1, 2, 3, move) || 
-    //    checkRow(4, 5, 6, move) || 
-    //    checkRow(7, 8, 9, move) ||
-    //    checkRow(1, 4, 7, move) ||
-    //    checkRow(2, 5, 8, move) ||
-    //    checkRow(3, 6, 9, move) ||
-    //    checkRow(1, 5, 9, move) ||
-    //    checkRow(3, 5, 7, move)) {
-    //
-    //    result = true;
-    //  }
-    //}
-    //
-    //  }
-    //
-    //  return result;
-    //}
+        // function to display message // 
+        function setMessage(msg) {
+            document.getElementById("message").innerText = msg;
+        }
+        // function for nextmove // 
+        function nextMove(square) {
+            if (document.winner != null) {
+                setMessage(document.winner + " already won the game.");
+            } else if (square.innerText == "") {
+                square.innerText = document.turn;
+                switchTurn();
+            } else {
+                setMessage("That square is already used.");
+            }
+        }
+        // Function for turn chnage // 
+        function switchTurn() {
+        
+            if (checkForWinner(document.turn)) {
+                setMessage("Congratulations, " + document.turn + "!  You win!");
+                document.winner = document.turn;
+            } else if (document.turn == "X") {
+                document.turn = "O";
+                setMessage("It's " + document.turn + "'s turn!");
+            } else {
+                document.turn = "X";
+                setMessage("It's " + document.turn + "'s turn!");
+            }
+        }
+        // function that checks for a winner // 
+        function checkForWinner(move) {
+            var result = false;
+            if (checkRow(1, 2, 3, move) || 
+                checkRow(4, 5, 6, move) ||
+                checkRow(7, 8, 9, move) || 
+                checkRow(1, 4, 7, move) ||
+                checkRow(2, 5, 8, move) ||
+                checkRow(3, 6, 9, move) ||
+                checkRow(1, 5, 9, move) ||
+                checkRow(3, 5, 7, move)) {
+                
+                result = true;
+            }
+            return result;
+        }
+        
+        function checkRow(a, b, c, move) {
+            var result = false;
+            if (getBox(a) == move && getBox(b) == move && getBox(c) == move) {
+                result = true;
+            }
+            return result;
+        }
+        
+        function getBox(number) {
+            return document.getElementById("s" + number).innerText;
+        }
+        
+        function clearBox(number) {
+            document.getElementById("s" + number).innerText = "";
+        }
